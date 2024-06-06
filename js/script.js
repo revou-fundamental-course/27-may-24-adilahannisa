8c9e6b6a319bc750b190c0a5d6c2cfd2736cb59c
adilahannisa <annisaadilah24@gmail.com>
2024-06-06 23:02:37 +0700

    inisiasi config

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 // Ini Javascript
+function validateForm() {
+    const namaPelanggan = document.getElementById("nama-pelanggan").value;
+    const banyakOrang = document.getElementById("banyak-orang").value;
+    const waktuReservasi= document.getElementById("waktu-reservasi").value;
+
+}
